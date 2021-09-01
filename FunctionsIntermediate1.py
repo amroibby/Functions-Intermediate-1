4c6556693fa6c95eb3a863bbf57c4b811383b571
Amro Ibrahim <amrofriend@yahoo.com>
2021-09-01 16:35:56 -0400

    final

---
+++ FunctionsIntermediate1.py
@@ -0,0 +1,57 @@
+# x = [ [5,2,3], [10,8,9] ] 
+# students = [
+#      {'first_name':  'Micheal', 'last_name' : 'Jordan'},
+#      {'first_name' : 'John', 'last_name' : 'Rosales'}
+# ]
+# sports_directory = {
+#     'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+#     'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+# }
+# z = [ {'x': 10, 'y': 20} ]
+
+# # # 1. 
+# # x[1][0] = 15
+# # print (x)
+
+# # # 2.
+# # students[0]['last_name'] = 'Bryant'
+# # print(students)
+
+# # # 3.
+# # sports_directory ['soccer'][0]="Andres"
+# # print(sports_directory)
+
+# # # 4.
+# # z [0]['y'] = 30
+# # print (z)
+
+# # 5.
+# students = [
+#     {'first_name': 'Michael', 'last_name': 'Jordan'},
+#     {'first_name': 'John', 'last_name': 'Rosales'},
+#     {'first_name': 'Mark', 'last_name': 'Guillen'},
+#     {'first_name': 'KB', 'last_name': 'Tonel'}
+# ]
+# def iterateDictionary(some_list):
+#     for x in some_list:
+#         print(f'first_name - {x["first_name"]}, last_name - {x["last_name"]}')
+    
+
+# iterateDictionary(students)
+
+# # 6.
+# students = [
+#     {'first_name': 'Michael', 'last_name': 'Jordan'},
+#     {'first_name': 'John', 'last_name': 'Rosales'},
+#     {'first_name': 'Mark', 'last_name': 'Guillen'},
+#     {'first_name': 'KB', 'last_name': 'Tonel'}
+# ]
+
+# def iterateDictionary2(key_name, some_list):
+#     for x in some_list:
+#         print(x[key_name])
+
+# answer1 =iterateDictionary2("first_name", students)
+# print()
+# answer2 =iterateDictionary2("last_name", students)
+
